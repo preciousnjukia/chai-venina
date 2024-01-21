740121ebcea89baee1796d539b96cb6e1789dbf0
L-JM3214 <lactorjm4@gmail.com>
2024-01-21 11:27:28 +0300

    Routing

--- client/vevinah/src/App.js
+++ client/vevinah/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
 import Menu from './components/Menu';
 import Orders from './components/Orders';
 import SignIn from './components/SignIn';
@@ -9,20 +8,22 @@ import Register from './components/SignUp';
 import PaymentPage from './components/PaymentPage';
 import TrackingPage from './components/TrackingPage';
 import HomePage from './components/HomePage';
-import Footer from './components/Footer';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-      <Route path="/navbar" element={<Navbar />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Orders />} />
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/sign_up" element={<Register />} />
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/tracking" element={<TrackingPage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </Router>
   );
