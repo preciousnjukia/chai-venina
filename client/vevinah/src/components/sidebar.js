37e09a2d34169045c928cdf05f140560c9bbbf1a
preciousnjukia <preciousnjukia@gmail.com>
2024-01-29 21:44:37 +0300

    Css updates

--- client/vevinah/src/components/sidebar.js
+++ client/vevinah/src/components/sidebar.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 function Sidebar() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -11,31 +18,35 @@ function Sidebar() {
 
   return (
     <div className="sidebar-wrapper">
-      <div className="sidebar-icon" onClick={toggleSidebar}>
-        &#9776; 
+      <div className={`menu-icon${isSidebarOpen ? ' open' : ''}`} onClick={toggleSidebar}>
+        <FontAwesomeIcon icon={faBars} />
       </div>
 
       {isSidebarOpen && (
         <div className="sidebar">
           <ul className='sidebar-list'>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/menu">
+              <FontAwesomeIcon icon={faUtensils} className='side-menu' />
+              Menu</Link>
               <hr className='sidebar-line' />
             </li>
             <li>
-              <Link to="/menu">Menu</Link>
+              <Link to="/about-us">
+              <FontAwesomeIcon icon={faInfoCircle} className='about-icon'/>
+              About</Link>
               <hr className='sidebar-line' />
             </li>
             <li>
-              <Link to="/about-us">About</Link>
+              <Link to="/contact-us">
+              <FontAwesomeIcon icon={faEnvelope} className='contact-icon'/>
+              Contact Us</Link>
               <hr className='sidebar-line' />
             </li>
             <li>
-              <Link to="/contact-us">Contact Us</Link>
-              <hr className='sidebar-line' />
-            </li>
-            <li>
-              <Link to="/cart">Orders</Link>
+              <Link to="/cart">
+              <FontAwesomeIcon icon={faShoppingCart} className='order-icon'/>
+              Orders</Link>
               <hr className='sidebar-line' />
             </li>
           </ul>
